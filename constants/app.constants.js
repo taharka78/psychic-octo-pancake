51f4898b203b24714c3d264293c4999252dfc095
taharka78 <ms.graphiste@gmail.com>
2022-05-23 12:01:44 +0200

    add error messages to app constants

--- constants/app.constants.js
+++ constants/app.constants.js
@@ -1,2 +1,3 @@
 const APP_ROMAN_TRANSLATIONS = {1:"I",4:"IV",5:"V",9:"IX",10:"X",40:"XL",50:"L",90 :"XC",100:"C"};
-module.exports = {APP_ROMAN_TRANSLATIONS}
+const ERROR_NUMBER_VALUE_SENT = { error : { message : "Please give a valid number"}};
+module.exports = {APP_ROMAN_TRANSLATIONS,ERROR_NUMBER_VALUE_SENT}
